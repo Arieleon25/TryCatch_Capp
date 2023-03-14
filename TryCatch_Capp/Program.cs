c57b09f8ebbc1d6dad4417cf003138722ae60577
Arieleon25 <Arieleon25@yahoo.com>
2023-03-14 15:25:03 -0400

    Update1

--- TryCatch_Capp/Program.cs
+++ TryCatch_Capp/Program.cs
@@ -10,36 +10,38 @@ namespace TryCatch_Capp
     {
         static void Main(string[] args)
         {
+            
             List<int> numList = new List<int>() { 4, 6, 9, 12, 20, 40, 50 };
             Console.WriteLine("Divide List numbers by: ");
-
-            foreach (int num in numList)
+           
+            try
             {
-                try
+                foreach (int num in numList)
                 {
                     int userInput = Convert.ToInt32(Console.ReadLine());
                     int total = num / userInput;
                     Console.WriteLine(total);
                 }
-
-                catch (FormatException)
-                {
-                    Console.WriteLine("Please type a whole number");
-                }
-                catch (DivideByZeroException)
-                {
-                    Console.WriteLine("please don't divide by zero");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                //finally
-                //{
-                //    Console.ReadLine();
-                //}
             }
-            Console.ReadLine();
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program has emerged from try/catch block and continued on with program execution.");
+                Console.ReadLine();
+            }
+            //Console.WriteLine("Program has emerged from try/catch block and continued on with program execution.");
+            //Console.ReadLine();
         }
     }
 }
